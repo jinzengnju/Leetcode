72d326c36fa484a7b83be8baf37ec49bce4620ea
jinzengnju <jzengscu@163.com>
2018-10-23 10:30:39 +0800

    update

--- src/DFS/MaxAreaofIsland.java
+++ src/DFS/MaxAreaofIsland.java
@@ -1,31 +1,49 @@
 package DFS;
 
-public class MaxAreaofIsland {
-    private int m,n;
-    private int[][] direction = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+//查找最大的连通面积
+//
+//695. Max Area of Island (Easy)
+//
+//[[0,0,1,0,0,0,0,1,0,0,0,0,0],
+// [0,0,0,0,0,0,0,1,1,1,0,0,0],
+// [0,1,1,0,1,0,0,0,0,0,0,0,0],
+// [0,1,0,0,1,1,0,0,1,0,1,0,0],
+// [0,1,0,0,1,1,0,0,1,1,1,0,0],
+// [0,0,0,0,0,0,0,0,0,0,1,0,0],
+// [0,0,0,0,0,0,0,1,1,1,0,0,0],
+// [0,0,0,0,0,0,0,1,1,0,0,0,0]]
 
-    public int maxAreaOfIsland(int[][] grid){
-        if(grid==null||grid.length==0)
+public class MaxAreaofIsland {
+    public static void main(String[] args){}
+
+    private int m;
+    private int n;
+    private int[][] direction = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    public int getRes(int[][] grids,int m,int n){
+        if (grids==null||grids.length==0){
             return 0;
-        m=grid.length;
-        n=grid[0].length;
-        int maxArea = 0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                maxArea=Math.max(maxArea,dfs(grid,i,j));
+        }
+        m=grids.length;
+        n=grids[0].length;
+        int maxArea=0;
+        for (int i=0;i<m;i++){
+            for (int j=0;j<n;j++){
+                maxArea=Math.max(maxArea,dfs(grids,i,j));
             }
         }
         return maxArea;
     }
-    private int dfs(int[][] grid,int r,int c){
-        if(r < 0 || r >= m || c < 0 || c >= n || grid[r][c] == 0){
+    private int dfs(int[][] grids,int row,int col){
+        if (row<0||row>=m||col<0||col>=m||grids[row][col]==0){
             return 0;
         }
-        grid[r][c]=0;
-        int area=1;
-        for(int[] d:direction){
-            area+=dfs(grid,r+d[0],c+d[1]);
+        grids[row][col]=0;//注意是递归，不是回溯
+        int area=1;//为什么每次递归函数都要设置area=1，因为下面的for循环中会有area累加
+        for (int[] d:direction){
+            area+=dfs(grids,row+d[0],col+d[1]);
         }
         return area;
     }
+
+
 }
