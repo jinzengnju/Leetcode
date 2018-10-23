1cd17111bad9a2eb7c7107d13b944ff2d5118f2d
jinzengnju <jzengscu@163.com>
2018-10-23 14:35:44 +0800

    update

---
+++ src/Matrix/MaxAreaofIsland.java
@@ -0,0 +1,49 @@
+package Matrix;
+
+//查找最大的连通面积
+//
+//695. Max Area of Island (Easy)
+//
+//[[0,0,1,0,0,0,0,1,0,0,0,0,0],
+// [0,0,0,0,0,0,0,1,1,1,0,0,0],
+// [0,1,1,0,1,0,0,0,0,0,0,0,0],
+// [0,1,0,0,1,1,0,0,1,0,1,0,0],
+// [0,1,0,0,1,1,0,0,1,1,1,0,0],
+// [0,0,0,0,0,0,0,0,0,0,1,0,0],
+// [0,0,0,0,0,0,0,1,1,1,0,0,0],
+// [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+
+public class MaxAreaofIsland {
+    public static void main(String[] args){}
+
+    private int m;
+    private int n;
+    private int[][] direction = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    public int getRes(int[][] grids,int m,int n){
+        if (grids==null||grids.length==0){
+            return 0;
+        }
+        m=grids.length;
+        n=grids[0].length;
+        int maxArea=0;
+        for (int i=0;i<m;i++){
+            for (int j=0;j<n;j++){
+                maxArea=Math.max(maxArea,dfs(grids,i,j));
+            }
+        }
+        return maxArea;
+    }
+    private int dfs(int[][] grids,int row,int col){
+        if (row<0||row>=m||col<0||col>=m||grids[row][col]==0){
+            return 0;
+        }
+        grids[row][col]=0;//注意是递归，不是回溯
+        int area=1;//为什么每次递归函数都要设置area=1，因为下面的for循环中会有area累加
+        for (int[] d:direction){
+            area+=dfs(grids,row+d[0],col+d[1]);
+        }
+        return area;
+    }
+
+
+}
---
+++ src/Matrix/MinimumPathSum.java
@@ -0,0 +1,49 @@
+package Matrix;
+
+//[[1,3,1],
+// [1,5,1],
+// [4,2,1]]
+//Given the above grid map, return 7. Because the path 1→3→1→1→1 minimizes the sum.
+//这道题不是用广度优先搜索，而是用动态规划
+
+public class MinimumPathSum {
+    public static void main(String[] args){}
+
+    public int getRes(int[][] grid){
+        if (grid==null||grid.length==0){
+            return 0;
+        }
+        int m=grid[0].length;
+        int[] dp=new int[m];
+        for (int i=0;i<m;i++){
+            dp[i]=grid[0][i];
+        }
+        for (int i=1;i<grid.length;i++){
+            dp[0]=grid[i][0];
+            for (int j=1;j<m;j++){
+                dp[j]=Math.min(dp[j-1],dp[j])+grid[i][j];
+            }
+        }
+        return dp[m-1];
+    }
+
+//    public int minPathSum(int[][] grid) {
+//        if (grid.length == 0 || grid[0].length == 0) {
+//            return 0;
+//        }
+//        int m = grid.length, n = grid[0].length;
+//        int[] dp = new int[n];
+//        for (int i = 0; i < m; i++) {
+//            for (int j = 0; j < n; j++) {
+//                if (i == 0) {
+//                    dp[j] = dp[j - 1];
+//                } else {
+//                    dp[j] = Math.min(dp[j - 1], dp[j]);
+//                }
+//                dp[j] += grid[i][j];
+//            }
+//        }
+//        return dp[n - 1];
+//    }
+
+}
